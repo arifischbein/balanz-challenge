c1ee98d7572cf27dfcf709c4205fec4d627a26fc
arifischbein <ariifischbein@gmail.com>
2022-08-20 16:11:42 -0300

    Android: cryptoCurrency view and disable nightMode

--- app/src/main/java/com/example/balanzchallenge/CurrencyActivity.kt
+++ app/src/main/java/com/example/balanzchallenge/CurrencyActivity.kt
@@ -2,12 +2,14 @@ package com.example.balanzchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CurrencyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_currency)
     }
 }
